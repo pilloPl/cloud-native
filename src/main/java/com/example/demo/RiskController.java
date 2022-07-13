912a9234a5bc6d00e4646680dd6412140591e341
jpilimon <jakub.pilimon@gmail.com>
2022-07-13 21:37:33 +0200

    circuit breaker done

--- src/main/java/com/example/demo/RiskController.java
+++ src/main/java/com/example/demo/RiskController.java
@@ -1,19 +1,42 @@
 package com.example.demo;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 public class RiskController {
 
     private final FailingRiskClient riskClient;
+    private final CircuitBreaker riskEvaluation;
 
-    public RiskController(FailingRiskClient riskClient) {
+    public RiskController(FailingRiskClient riskClient, CircuitBreakerFactory factory) {
         this.riskClient = riskClient;
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig
+                .custom()
+                .failureRateThreshold(5)
+                .waitDurationInOpenState(Duration.ofMillis(300))
+                .slidingWindowSize(2)
+                .build();
+
+        TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofMillis(300))
+                .build();
+
+        factory.configureDefault(id -> new Resilience4JConfigBuilder("config").circuitBreakerConfig(circuitBreakerConfig)
+                .timeLimiterConfig(timeLimiterConfig).build());
+
+        this.riskEvaluation = factory.create("riskEvaluation");
     }
 
     @GetMapping("/evaluate")
     String evaluate() {
-        return riskClient.evaluate();
+        return riskEvaluation.run(riskClient::evaluate, throwable -> "FALLBACK");
     }
 }
