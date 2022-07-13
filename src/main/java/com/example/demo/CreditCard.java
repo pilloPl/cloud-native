49ed9336cb1cc747fe77d33d05c4a88fb6f505c5
jpilimon <jakub.pilimon@gmail.com>
2022-07-13 20:58:56 +0200

    ex: data1

--- src/main/java/com/example/demo/CreditCard.java
+++ src/main/java/com/example/demo/CreditCard.java
@@ -5,18 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
-@Entity
 public class CreditCard {
 
-    @Id
-    @GeneratedValue
+
     private Long id;
 
-    private BigDecimal balance;
-
-    CreditCard(BigDecimal balance) {
-        this.balance = balance;
-    }
 
     public CreditCard() {
     }
